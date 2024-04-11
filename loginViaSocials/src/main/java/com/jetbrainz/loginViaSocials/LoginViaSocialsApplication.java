0f4a057e202fb20c3373d09c7a5842a10ea70cb7
Taha GHAILAN <91258092+tahag17@users.noreply.github.com>
2024-04-11 18:42:00 +0000

    First Commit

---
+++ loginViaSocials/src/main/java/com/jetbrainz/loginViaSocials/LoginViaSocialsApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrainz.loginViaSocials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginViaSocialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginViaSocialsApplication.class, args);
+	}
+
+}
---
+++ loginViaSocials/src/test/java/com/jetbrainz/loginViaSocials/LoginViaSocialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrainz.loginViaSocials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginViaSocialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
